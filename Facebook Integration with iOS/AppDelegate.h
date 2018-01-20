84807618700a04c75a4e6ccbe25b1bb7e0f32450
Meet Shah <meet86@gmail.com>
2018-01-19 23:18:35 -0600

    Initial Commit

---
+++ Facebook Integration with iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Facebook Integration with iOS
+//
+//  Created by Meet Shah on 1/19/18.
+//  Copyright © 2018 Meet Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
